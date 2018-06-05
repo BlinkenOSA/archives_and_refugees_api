2d3a3bf87c824c30344183cf2507838cbb48267f
JoshBone <jozsef.bone@gmail.com>
2018-06-05 09:04:39 +0200

    Added endpoints for the temporal coverage min-max.

--- records/urls.py
+++ records/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^keywords/$', KeywordListView.as_view(), name='keywords'),
     url(r'^directors/$', DirectorListView.as_view(), name='directors'),
     url(r'^films/$', FilmLibraryRecordListView.as_view(), name='films'),
+    url(r'^films/temporal_coverage/$', FilmDateRangeView.as_view(), name='films_temporal_coverage')
 ]
 
--- records/views.py
+++ records/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.shortcuts import render
+from rest_framework.response import Response
 
 # Create your views here.
 from rest_framework.generics import ListAPIView
 from django_filters import rest_framework as filters
+from rest_framework.views import APIView
 
 from records.models import Country, Keyword, Director, FilmLibraryRecord
 from records.serializers import CountrySerializer, KeywordSerializer, DirectorSerializer, FilmLibraryRecordSerializer
@@ -33,12 +35,24 @@ class DirectorListView(ListAPIView):
 class FilmLibraryRecordFilterClass(filters.FilterSet):
     keyword = filters.ModelChoiceFilter(label='Keyword', queryset=Keyword.objects.all(), method='filter_keyword')
     country = filters.ModelChoiceFilter(label='Country', queryset=Country.objects.all(), method='filter_country')
+    date_from = filters.NumberFilter(label='Temporal Coverage From', method='filter_date_from')
+    date_to = filters.NumberFilter(label='Temporal Coverage To', method='filter_date_to')
 
     def filter_country(self, queryset, name, value):
         return queryset.filter(countries=value)
 
     def filter_keyword(self, queryset, name, value):
         return queryset.filter(keywords=value)
+
+    def filter_date_from(self, queryset, name, value):
+        return queryset.exclude(
+            Q(temporal_coverage_end__lt=value)
+        )
+
+    def filter_date_to(self, queryset, name, value):
+        return queryset.exclude(
+            Q(temporal_coverage_start__gt=value)
+        )
 
     class Meta:
         model = FilmLibraryRecord
@@ -50,3 +64,10 @@ class FilmLibraryRecordListView(ListAPIView):
     queryset = FilmLibraryRecord.objects.all().order_by('title')
     filter_backends = (filters.DjangoFilterBackend,)
     filter_class = FilmLibraryRecordFilterClass
+
+
+class FilmDateRangeView(APIView):
+    def get(self, request, format=None):
+        earliest_film = FilmLibraryRecord.objects.filter(temporal_coverage_start__isnull=False).order_by('temporal_coverage_start').first()
+        latest_film = FilmLibraryRecord.objects.filter().order_by('-temporal_coverage_end').first()
+        return Response({'earliest': earliest_film.temporal_coverage_start, 'latest': latest_film.temporal_coverage_end})
