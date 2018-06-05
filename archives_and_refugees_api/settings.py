aada9f2982239ffc107929b79b7b6fab2539a0ba
JoshBone <jozsef.bone@gmail.com>
2018-06-05 11:39:26 +0200

    Add STATIC_ROOT

--- archives_and_refugees_api/settings.py
+++ archives_and_refugees_api/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'archives_and_refugees/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
+
 
 try:
     from local_settings import *
